7dac7aac2a0145e1c558a43249505bd6d65560b4
ddauber <62331069+ddauber@users.noreply.github.com>
2021-08-04 15:49:12 +0100

    Updated documentation for install_r4np()

--- R/install_r4np.R
+++ R/install_r4np.R
@@ -2,17 +2,17 @@
 #'
 #' @details Installs the following R packages at once:
 ##' \itemize{
-##'  \item{tidyverse: }{A package to install the core tidyverse packages.}
-##'  \item{janitor: }{A package to help clean data, e.g. colum names, etc.}
-##'  \item{naniar: }{A package to inspect and handle missing data.}
-##'  \item{psych: }{A acpakge to compute all kinds of statistics relevant to Social Sciences.}
-##'  \item{skimr: }{A quick way to retrieve descriptive statistics and 'skim' your data.}
-##'  \item{correlation: }{A package with advanced functionality to perform correlations.}
-##'  \item{rcompanion: }{A package with additional functions, such as for effect sizes.}
-##'  \item{effectsize: }{A package that helps to compute and interpret effect sizes.}
-##'  \item{car: }{A package to help interpret regression results}
+##'  \item{tidyverse: }{Install the core tidyverse packages.}
+##'  \item{janitor: }{Clean data, e.g. colum names, etc.}
+##'  \item{naniar: }{Inspect and handle missing data.}
+##'  \item{psych: }{Compute all kinds of statistics relevant to Social Sciences.}
+##'  \item{skimr: }{Retrieve descriptive statistics and 'skim' your data.}
+##'  \item{correlation: }{Advanced functionality to perform correlations.}
+##'  \item{rcompanion: }{Additional functions, such as for effect sizes.}
+##'  \item{effectsize: }{Compute and interpret effect sizes.}
+##'  \item{car: }{Interpret regression results}
 ##'  \item{FSA: }{NOT SURE}
-##'  \item{jtools: }{A package which helps to retrieve nicely formatted output from regressions.}
+##'  \item{jtools: }{Retrieve nicely formatted output from regressions.}
 ##'  }
 #' @examples
 #' \dontrun{
@@ -22,17 +22,17 @@
 #' @export
 install_r4np <- function() {
   utils::install.packages(c(
-    "tidyverse",     # package for using the tidyverse
-    "janitor",       # clean names of columns. #data-wrangling
-    "naniar",        # mnissing data
-    "psych",         # descriptive stats, Cronbach's alpha and general stats
-    "skimr",         # inspect your data quickly and easily
-    "correlation",   # compute correlations
-    "rcompanion",    # some more effect sizes not included in 'effectsize'
-    "effectsize",    # compute and check various effect sizes
-    "car",           # check whether it is really needed
-    "FSA",           # check whether it is really needed
-    "jtools"         # show regression results as nicely formatted output
+    "tidyverse",
+    "janitor",
+    "naniar",
+    "psych",
+    "skimr",
+    "correlation",
+    "rcompanion",
+    "effectsize",
+    "car",
+    "FSA",
+    "jtools"
     # Add packages related to mixed-methods research
   ))
 
