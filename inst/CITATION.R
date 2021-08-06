d2e5a0ea122014657eaccf3bf44ac05b5f86beec
ddauber <62331069+ddauber@users.noreply.github.com>
2021-08-06 11:18:26 +0100

    Added citation file.

---
+++ inst/CITATION.R
@@ -0,0 +1,10 @@
+citHeader("To cite r4np in publications, please use:")
+
+citEntry(entry = "book",
+         author = "Daniel Dauber",
+         title = "R for Non-Programmers: A Guide for Social Scientists",
+         publisher = "Bookdown.org",
+         year = "2021",
+         url = "https://daniel.dauber.com",
+         textVersion = "Dauber, Daniel (2021). R for Non-Programmers: A Guide for Social Scientists. Bookdown.org, https://daniel.dauber.com"
+)
